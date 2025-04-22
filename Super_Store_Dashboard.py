42698f9dd850bfbb5c236b63afc1de2906092e9d
aidanalyst <ai.danalyst@gmail.com>
2025-04-22 22:48:19 +0100

    adding sitebar filter1

--- Super_Store_Dashboard.py
+++ Super_Store_Dashboard.py
@@ -5,3 +5,52 @@ import os
 import warnings
 warnings.filterwarnings("ignore")
 
+st.set_page_config(page_title="Super Store Dashboard", page_icon=":bar_chart:", layout="wide")
+st.title(" :bar_chart: Super Store EDA")
+st.markdown('<style>div.block-container{padding-top: 2rem;}</style>', unsafe_allow_html=True)
+
+fl = st.file_uploader(":file_folder: Upload your CSV file", type=["csv", "txt", "xlsx", "xls"])
+if fl is not None:
+    filename = fl.name
+    st.write(filename)
+    df = pd.read_csv(fl, encoding="ISO-8859-1")
+else:
+    os.chdir(r"C:\Users\AI NetMan\Documents\GitHub\Super_Store")
+    df = pd.read_csv("Superstore.csv", encoding = "ISO-8859-1")
+
+col1, col2 = st.columns((2))
+df["Order Date"] = pd.to_datetime(df["Order Date"])
+
+#Getting the min and max date from the dataset
+startDate = pd.to_datetime(df["Order Date"]).min()
+endDate = pd.to_datetime(df["Order Date"]).max()
+
+with col1:
+    date1 = pd.to_datetime(st.date_input("Start Date", startDate))
+
+with col2:
+    date2 = pd.to_datetime(st.date_input("End Date", endDate))
+
+# Filter the dataframe based on the selected date range
+df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
+
+st.sidebar.header("Filter Options")
+st.sidebar.markdown("### Select the filters you want to apply")
+
+# Create for region
+region = st.sidebar.multiselect("Region", df["Region"].unique())
+if not region:
+    df2 = df.copy()
+else:
+    df2 = df[df["Region"].isin(region)]
+
+# Create for state
+state = st.sidebar.multiselect("State", df2["State/Province"].unique())
+if not state:
+    df3 = df2.copy()
+else:
+    df3 = df2[df2["State/Province"].isin(state)] 
+
+# Create for city
+city = st.sidebar.multiselect("City", df3["City"].unique())
+
